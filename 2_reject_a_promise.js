805d1757a7a146babb84318dbbbdf807042194ee
AlexKVal <alexkval@gmail.com>
2015-03-30 15:12:09 +0300

    2_reject_a_promise.js

---
+++ 2_reject_a_promise.js
@@ -0,0 +1,8 @@
+var q = require('q');
+var defer = q.defer();
+
+defer.promise.then(null, function (error) {
+  console.log(error.message);
+})
+
+setTimeout(defer.reject, 300, new Error('REJECTED!'))
